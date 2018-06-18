11fc681640eb05fcfe9c3cdfcc69ced7cb1a87c3
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-19 01:42:30 +0530

    Cart DAO !

--- OnlineShopping/src/main/java/com/tyagi/project/OnlineShopping/dao/SupplierDAO.java
+++ OnlineShopping/src/main/java/com/tyagi/project/OnlineShopping/dao/SupplierDAO.java
@@ -10,46 +10,39 @@ import org.springframework.stereotype.Repository;
 
 import com.tyagi.project.OnlineShopping.model.Supplier;
 
-
-
 @Repository("supplierDAO")
-	public class SupplierDAO 
-	{
+public class SupplierDAO {
 	@Autowired
 	SessionFactory sessionFactory;
-	public SupplierDAO(SessionFactory sessionFactory)
-	{
-		this.sessionFactory=sessionFactory;
+
+	public SupplierDAO(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
 	}
+
 	@Transactional
-	public void insertUpdateSupplier(Supplier supplier)
-	{
-		Session session=sessionFactory.getCurrentSession();
+	public void insertUpdateSupplier(Supplier supplier) {
+		Session session = sessionFactory.getCurrentSession();
 		session.saveOrUpdate(supplier);
 	}
 
-	public Supplier getSupplier(int suppid)
-	{
-		Session session=sessionFactory.openSession();
-		Supplier supplier=(Supplier)session.get(Supplier.class,suppid);
+	public Supplier getSupplier(int suppid) {
+		Session session = sessionFactory.openSession();
+		Supplier supplier = (Supplier) session.get(Supplier.class, suppid);
 		session.close();
 		return supplier;
 	}
+
 	@Transactional
-	public void deleteSupplier(Supplier supplier)
-	{
+	public void deleteSupplier(Supplier supplier) {
 		sessionFactory.getCurrentSession().delete(supplier);
 	}
 
-	public List<Supplier> getSupplierDetails()
-	{
-		Session session=sessionFactory.openSession();
-		Query query=session.createQuery("from Supplier");
-		List<Supplier> list=query.list();
+	public List<Supplier> getSupplierDetails() {
+		Session session = sessionFactory.openSession();
+		Query query = session.createQuery("from Supplier");
+		List<Supplier> list = query.list();
 		session.close();
 		return list;
 	}
 
-
-
-	}
+}
--- OnlineShopping/src/test/java/com/tyagi/project/OnlineShopping/SupplierTestCase.java
+++ OnlineShopping/src/test/java/com/tyagi/project/OnlineShopping/SupplierTestCase.java
@@ -1,6 +1,5 @@
 package com.tyagi.project.OnlineShopping;
 
-
 import java.util.List;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,52 +8,54 @@ import com.tyagi.project.OnlineShopping.model.Supplier;
 
 import javax.naming.Context;
 
-
 public class SupplierTestCase {
-	
+
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 		context.scan("com.tyagi.project.OnlineShopping");
 		context.refresh();
 
-	SupplierDAO supplierDAO=(SupplierDAO)context.getBean("supplierDAO");
+		SupplierDAO supplierDAO = (SupplierDAO) context.getBean("supplierDAO");
 
-		
-	Supplier supplier=new Supplier();
+		Supplier supplier = new Supplier();
 
-	supplier.setSuppname("Aman");
-	supplier.setSuppaddress("Ghaziabad");
+		supplier.setSuppname("Aman");
+		supplier.setSuppaddress("Ghaziabad");
 
-	supplierDAO.insertUpdateSupplier(supplier);		
-	System.out.println("Supplier Inserted");
+		supplierDAO.insertUpdateSupplier(supplier);
+		System.out.println("Supplier Inserted");
 
+		// Retrieval TestCase
+		/*
+		 * Supplier supplier=supplierDAO.getSupplier(2);
+		 * System.out.println("Supplier Name:"+supplier.getSuppname());
+		 * System.out.println("Supplier Description:"+supplier.getSuppaddress());
+		 */
 
-	        //Retrieval TestCase
-			/*Supplier supplier=supplierDAO.getSupplier(2);
-			System.out.println("Supplier Name:"+supplier.getSuppname());
-			System.out.println("Supplier Description:"+supplier.getSuppaddress());*/
-			
-			//Deletion TestCase
-			/*Supplier supplier=supplierDAO.getSupplier(2);
-			supplierDAO.deleteSupplier(supplier);
-			System.out.println("The Supplier Deleted");*/
-			
-			//Retrieving the Data
-			
-			/*List<Supplier> list=supplierDAO.getSupplierDetails();
-			
-			for(Supplier supplier1:list)
-			{
-				System.out.println(supplier1.getSuppid()+":"+supplier1.getSuppname()+":"+supplier1.getSuppaddress());
-			}*/
-			
-			//Update the Supplier
-			/*Supplier supplier=supplierDAO.getSupplier(1);
-			supplier.setSuppname("X");
-			supplierDAO.insertUpdateSupplier(supplier);
-			System.out.println("The Supplier Updated");*/
-		
-	       }
+		// Deletion TestCase
+		/*
+		 * Supplier supplier=supplierDAO.getSupplier(2);
+		 * supplierDAO.deleteSupplier(supplier);
+		 * System.out.println("The Supplier Deleted");
+		 */
+
+		// Retrieving the Data
+
+		/*
+		 * List<Supplier> list=supplierDAO.getSupplierDetails();
+		 * 
+		 * for(Supplier supplier1:list) {
+		 * System.out.println(supplier1.getSuppid()+":"+supplier1.getSuppname()+":"+
+		 * supplier1.getSuppaddress()); }
+		 */
+
+		// Update the Supplier
+		/*
+		 * Supplier supplier=supplierDAO.getSupplier(1); supplier.setSuppname("X");
+		 * supplierDAO.insertUpdateSupplier(supplier);
+		 * System.out.println("The Supplier Updated");
+		 */
+
+	}
 
 }
-
